54cfe725c6bc7d86f609c6167a05046d2e4e34d0
BrainBrian <brian@brainbrian.com>
2014-07-09 14:53:40 -0700

    added alignment property to overlay image on feature slider

--- _/inc/modules/featured-slider.php
+++ _/inc/modules/featured-slider.php
@@ -9,9 +9,10 @@
 							$image = get_sub_field('gnu_feature_slider_img');
 							$overlayImage = get_sub_field('gnu_feature_slider_overlay_img');
 							$overlayImageAlt = get_sub_field('gnu_feature_slider_overlay_img_alt');
+							$overlayImageAlign = get_sub_field('gnu_feature_slider_overlay_img_align');
 					?>
 					<div class="slide-item owl-lazy" data-src="<?php echo $image['url']; ?>" data-src-retina="<?php echo $image['url']; ?>">
-						<div class="slide-details right">
+						<div class="slide-details <?php echo $overlayImageAlign; ?>">
 							<div class="slide-overlay">
 								<img src="<?php echo $overlayImage['url']; ?>" alt="<?php echo $overlayImageAlt; ?>" />
 							</div>

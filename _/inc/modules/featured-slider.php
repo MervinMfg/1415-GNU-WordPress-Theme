d5bcaf6bf095b018b9ab0e3d9ca9ba50b7afe340
BrainBrian <brian@brainbrian.com>
2014-07-09 15:30:58 -0700

    updated featured slider to include url link

--- _/inc/modules/featured-slider.php
+++ _/inc/modules/featured-slider.php
@@ -10,13 +10,16 @@
 							$overlayImage = get_sub_field('gnu_feature_slider_overlay_img');
 							$overlayImageAlt = get_sub_field('gnu_feature_slider_overlay_img_alt');
 							$overlayImageAlign = get_sub_field('gnu_feature_slider_overlay_img_align');
+							$link = get_sub_field('gnu_feature_slider_url');
 					?>
 					<div class="slide-item owl-lazy" data-src="<?php echo $image['url']; ?>" data-src-retina="<?php echo $image['url']; ?>">
-						<div class="slide-details <?php echo $overlayImageAlign; ?>">
-							<div class="slide-overlay">
-								<img src="<?php echo $overlayImage['url']; ?>" alt="<?php echo $overlayImageAlt; ?>" />
+						<a href="<?php echo $link; ?>">
+							<div class="slide-details <?php echo $overlayImageAlign; ?>">
+								<div class="slide-overlay">
+									<img src="<?php echo $overlayImage['url']; ?>" alt="<?php echo $overlayImageAlt; ?>" />
+								</div>
 							</div>
-						</div>
+						</a>
 					</div><!-- .slide-item -->
 					<?php endwhile; ?>
 				</div><!-- .slide-list -->

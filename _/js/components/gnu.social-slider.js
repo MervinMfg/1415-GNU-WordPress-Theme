f3d6606bfade6556cdc08aa0fa8157decba8c6f1
BrainBrian <brian@brainbrian.com>
2014-09-18 19:31:55 -0700

    added team detail page and made proper modifications to social slider.

--- _/js/components/gnu.social-slider.js
+++ _/js/components/gnu.social-slider.js
@@ -9,9 +9,10 @@ GNU.SocialSlider = function () {
 	this.config = {
 		carousel: null,
 		instagramUsername: null,
-		instagramPosts: null,
+		instagramPosts: 0,
 		facebookUsername: null,
-		facebookPosts: null
+		facebookPosts: 0,
+		completedLoads: 0
 	};
 	this.init();
 };
@@ -25,88 +26,108 @@ GNU.SocialSlider.prototype = {
 	loadInstagram: function () {
 		var self = this;
 		// INSTAGRAM POSTS
-		self.config.instagramUsername = $('.social-slider').attr('data-instagram');
-		self.config.instagramPosts = [];
-		$.ajax({
-			dataType: "json",
-			url: '/feeds/instagram/?username=' + self.config.instagramUsername + '&limit=4',
-			success: function (photosJSON) {
-				var photosData = photosJSON.data;
-				for (var i = 0; i < photosData.length; i++) {
-					var photoData, postItem;
-					photoData = photosData[i];
-					// set up instagram item
-					postItem = '<div class="social-item instagram"><a href="' + photoData.link + '" target="_blank" class="owl-lazy" data-src="' + photoData.images.low_resolution.url + '" title="' + photoData.caption.text + '"><div class="item-icon"><div class="icon"></div></div></a></div>';
-					self.config.instagramPosts.push(postItem);
+		if ($('.social-slider').attr('data-instagram') !== "") {
+			self.config.instagramUsername = $('.social-slider').attr('data-instagram');
+			self.config.instagramPosts = [];
+			$.ajax({
+				dataType: "json",
+				url: '/feeds/instagram/?username=' + self.config.instagramUsername + '&limit=4',
+				success: function (photosJSON) {
+					var photosData = photosJSON.data;
+					for (var i = 0; i < photosData.length; i++) {
+						var photoData, postItem;
+						photoData = photosData[i];
+						// set up instagram item
+						postItem = '<div class="social-item instagram"><a href="' + photoData.link + '" target="_blank" class="owl-lazy" data-src="' + photoData.images.low_resolution.url + '" title="' + photoData.caption.text + '"><div class="item-icon"><div class="icon"></div></div></a></div>';
+						self.config.instagramPosts.push(postItem);
+					}
+					// randomize/shuffle instagram posts with underscore.js
+					self.config.instagramPosts = _.shuffle(self.config.instagramPosts);
+					self.loadComplete();
 				}
-				// randomize/shuffle instagram posts with underscore.js
-				self.config.instagramPosts = _.shuffle(self.config.instagramPosts);
-				self.loadComplete();
-			}
-		});
+			});
+		} else {
+			self.loadComplete(); // trigger load even though not complete
+		}
 	},
 	loadFacebook: function () {
 		var self = this;
 		// FACEBOOK POSTS
-		self.config.facebookUsername = $('.social-slider').attr('data-facebook');
-		self.config.facebookPosts = [];
-		$.ajax({
-			dataType: "json",
-			url: '/feeds/facebook/?username=' + self.config.facebookUsername + '&limit=16',
-			success: function (postsJSON) {
-				var postsData;
-				postsData = postsJSON.data;
-				for (var i = 0; i < postsData.length; i++) {
-					var postData, postItem;
-					postData = postsData[i];
-					if (postData.type != "status") {
-						var postDate, monthArray, postImage, postMessage;
-						// process date stamp
-						postDate = postData.created_time;
-						monthArray = {
-							Jan: "January", Feb: "February", Mar: "March", Apr: "April", May: "May", Jun: "June", Jul: "July", Aug: "August", Sep: "September", Oct: "October", Nov: "November", Dec: "December"
-						};
-						// make friendly for Safari and IE
-						postDate = postDate.replace(/-/g, '/').replace(/T/, ' ').replace(/\+/, ' +');
-						// create date string
-						postDate = String(new Date(postDate)).replace(
-							/\w{3} (\w{3}) (\d{2}) (\d{4}) (\d{2}):(\d{2}):[^(]+\(([A-Z]{3})\)/,
-							function ($0, $1, $2, $3, $4, $5, $6) {
-								return monthArray[$1] + " " + $2 + ", " + $3; //+ " - " + $4%12 + ":" + $5 + ( + $4 > 12 ? "PM" : "AM") + " " + $6 hide time and date
-							}
-						);
-						// if still invalid, make it blank
-						if(postDate == "Invalid Date") postDate = "";
-						// get larger picture
-						postImage = postData.picture;
-						postImage = postImage.replace("_s", "_n");
-						// get message
-						postMessage = postData.message;
-						if(postMessage === undefined) postMessage = "";
-						// set up facebook item
-						postItem = '<a href="' + postData.link + '" target="_blank"><div class="item-icon"><div class="icon"></div></div><div class="item-photo"><img src="' + GNU.Main.config.wpImgPath + 'square.gif" alt="" class="lazy" data-src="' + postImage + '" /></div><div class="item-copy"><p class="fb-page">' + postData.from.name + ' on Facebook</p><p class="fb-date">' + postDate + '</p><p class="fb-excerpt">' + postMessage + '</p></div></a>';
-						self.config.facebookPosts.push(postItem);
+		if ($('.social-slider').attr('data-facebook') !== "") {
+			self.config.facebookUsername = $('.social-slider').attr('data-facebook');
+			self.config.facebookPosts = [];
+			$.ajax({
+				dataType: "json",
+				url: '/feeds/facebook/?username=' + self.config.facebookUsername + '&limit=16',
+				success: function (postsJSON) {
+					var postsData;
+					postsData = postsJSON.data;
+					for (var i = 0; i < postsData.length; i++) {
+						var postData, postItem;
+						postData = postsData[i];
+						if (postData.type != "status") {
+							var postDate, monthArray, postImage, postMessage;
+							// process date stamp
+							postDate = postData.created_time;
+							monthArray = {
+								Jan: "January", Feb: "February", Mar: "March", Apr: "April", May: "May", Jun: "June", Jul: "July", Aug: "August", Sep: "September", Oct: "October", Nov: "November", Dec: "December"
+							};
+							// make friendly for Safari and IE
+							postDate = postDate.replace(/-/g, '/').replace(/T/, ' ').replace(/\+/, ' +');
+							// create date string
+							postDate = String(new Date(postDate)).replace(
+								/\w{3} (\w{3}) (\d{2}) (\d{4}) (\d{2}):(\d{2}):[^(]+\(([A-Z]{3})\)/,
+								function ($0, $1, $2, $3, $4, $5, $6) {
+									return monthArray[$1] + " " + $2 + ", " + $3; //+ " - " + $4%12 + ":" + $5 + ( + $4 > 12 ? "PM" : "AM") + " " + $6 hide time and date
+								}
+							);
+							// if still invalid, make it blank
+							if(postDate == "Invalid Date") postDate = "";
+							// get larger picture
+							postImage = postData.picture;
+							postImage = postImage.replace("_s", "_n");
+							// get message
+							postMessage = postData.message;
+							if(postMessage === undefined) postMessage = "";
+							// set up facebook item
+							postItem = '<a href="' + postData.link + '" target="_blank"><div class="item-icon"><div class="icon"></div></div><div class="item-photo"><img src="' + GNU.Main.config.wpImgPath + 'square.gif" alt="" class="lazy" data-src="' + postImage + '" /></div><div class="item-copy"><p class="fb-page">' + postData.from.name + ' on Facebook</p><p class="fb-date">' + postDate + '</p><p class="fb-excerpt">' + postMessage + '</p></div></a>';
+							self.config.facebookPosts.push(postItem);
+						}
 					}
+					self.loadComplete();
 				}
-				self.loadComplete();
-			}
-		});
+			});
+		} else {
+			self.loadComplete(); // trigger load even though not complete
+		}
 	},
 	loadComplete: function () {
 		var self = this;
-		// Check if both facebook and insta have loaded, and if so render the carousel
-		if(self.config.instagramPosts.length > 0 && self.config.facebookPosts.length > 1) {
-			i = 0; // used for incrimenting through the dom elements
-			while(self.config.instagramPosts.length > 0 && self.config.facebookPosts.length > 1) {
-				$('.social-slider .social-list').append(self.config.instagramPosts[0]);
-				self.config.instagramPosts.shift();
-				$('.social-slider .social-list').append('<div class="social-item facebook">' + self.config.facebookPosts[0] + self.config.facebookPosts[1] + '</div>');
-				self.config.facebookPosts.shift();
-				self.config.facebookPosts.shift();
-				// move slides to the correct location
-				$('.social-slider .social-list .instagram:eq(' + i + ')').insertBefore($('.social-slider .social-list .vimeo:eq(' + i + ')'));
-				$('.social-slider .social-list .facebook:eq(' + i + ')').insertBefore($('.social-slider .social-list .vimeo:eq(' + i + ')'));
-				i++;
+		// incriment # of loads completed
+		self.config.completedLoads ++;
+		// make sure both insta and fb have loaded, or at least been called
+		if (self.config.completedLoads > 1) {
+			// Check if both facebook and insta have loaded, and if so render the carousel
+			if (self.config.instagramPosts.length > 0) {
+				i = 0; // used for incrimenting through the dom elements
+				while(self.config.instagramPosts.length > 0) {
+					$('.social-slider .social-list').append(self.config.instagramPosts[0]);
+					self.config.instagramPosts.shift();
+					// move slides to the correct location
+					$('.social-slider .social-list .instagram:eq(' + i + ')').insertBefore($('.social-slider .social-list .vimeo:eq(' + i + ')'));
+					i++;
+				}
+			}
+			if (self.config.facebookPosts.length > 1) {
+				i = 0; // used for incrimenting through the dom elements
+				while(self.config.facebookPosts.length > 1) {
+					$('.social-slider .social-list').append('<div class="social-item facebook">' + self.config.facebookPosts[0] + self.config.facebookPosts[1] + '</div>');
+					self.config.facebookPosts.shift();
+					self.config.facebookPosts.shift();
+					// move slides to the correct location
+					$('.social-slider .social-list .facebook:eq(' + i + ')').insertBefore($('.social-slider .social-list .vimeo:eq(' + i + ')'));
+					i++;
+				}
 			}
 			// done loading, so show the items
 			$(".social-slider .social-list").removeClass('loading');
@@ -153,6 +174,7 @@ GNU.SocialSlider.prototype = {
 		});
 	},
 	initVideo: function (videoID, videoTitle) {
+		console.log('init video');
 		$('.social-slider').addClass('video');
 		$('.social-slider .video-player .video-title').html(videoTitle);
 		$('.social-slider .video-player .video-wrapper').html('<iframe src="http://player.vimeo.com/video/' + videoID + '?title=0&amp;byline=0&amp;portrait=0&amp;color=99CC33&amp;autoplay=1&amp;loop=1" width="640" height="360" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>');
--- single-gnu_team.php
+++ single-gnu_team.php
@@ -5,16 +5,27 @@ Template Name: Team Detail
 	get_header();
 	if (have_posts()) : while (have_posts()) : the_post();
 ?>
-			<h1><?php the_title(); ?></h1>
-			<?php if(get_field('gnu_team_personal_website')) : ?><a href="<?php the_field('gnu_team_personal_website'); ?>" class="personal-site" target="_blank">Personal Website</a><?php endif; ?>
-			<?php the_content(); ?>
+			<section class="team-details">
+				<div class="team-content">
+					<h1 class="team-title"><?php the_title(); ?></h1>
+					<h5 class="team-subtitle"><?php echo get_the_content(); ?></h5>
+					<ul class="social-icons black nav-menu">
+						<?php if(get_field('gnu_team_facebook_username')) : ?><li class="menu-item"><a href="http://www.facebook.com/<?php the_field('gnu_team_facebook_username'); ?>" class="icon-facebook" target="_blank"><span class="offscreen">Facebook</span></a></li><?php endif; ?>
+						<?php if(get_field('gnu_team_twitter_username')) : ?><li class="menu-item"><a href="http://twitter.com/<?php the_field('gnu_team_twitter_username'); ?>" class="icon-twitter" target="_blank"><span class="offscreen">Twitter</span></a></li><?php endif; ?>
+						<?php if(get_field('gnu_team_vimeo_username')) : ?><li class="menu-item"><a href="http://vimeo.com/<?php the_field('gnu_team_vimeo_username'); ?>" class="icon-vimeo" target="_blank"><span class="offscreen">Vimeo</span></a></li><?php endif; ?>
+						<?php if(get_field('gnu_team_instagram_username')) : ?><li class="menu-item"><a href="http://instagram.com/<?php the_field('gnu_team_instagram_username'); ?>" class="icon-instagram" target="_blank"><span class="offscreen">Instagram</span></a></li><?php endif; ?>
+					</ul>
+					<?php if(get_field('gnu_team_personal_website')) : ?><p class="team-website"><a href="<?php the_field('gnu_team_personal_website'); ?>" target="_blank">Personal Website</a></p><?php endif; ?>
+				</div>
 
-			<ul class="social-icons black nav-menu">
-				<?php if(get_field('gnu_team_facebook_username')) : ?><li class="menu-item"><a href="http://www.facebook.com/<?php the_field('gnu_team_facebook_username'); ?>" class="icon-facebook" target="_blank"><span class="offscreen">Facebook</span></a></li><?php endif; ?>
-				<?php if(get_field('gnu_team_twitter_username')) : ?><li class="menu-item"><a href="http://twitter.com/<?php the_field('gnu_team_twitter_username'); ?>" class="icon-twitter" target="_blank"><span class="offscreen">Twitter</span></a></li><?php endif; ?>
-				<?php if(get_field('gnu_team_vimeo_username')) : ?><li class="menu-item"><a href="http://vimeo.com/<?php the_field('gnu_team_vimeo_username'); ?>" class="icon-vimeo" target="_blank"><span class="offscreen">Vimeo</span></a></li><?php endif; ?>
-				<?php if(get_field('gnu_team_instagram_username')) : ?><li class="menu-item"><a href="http://instagram.com/<?php the_field('gnu_team_instagram_username'); ?>" class="icon-instagram" target="_blank"><span class="offscreen">Instagram</span></a></li><?php endif; ?>
-			</ul>
+				<?php if (get_field('gnu_team_profile_photo')) : ?>
+
+				<div class="team-photo"><img src="<?php echo get_field('gnu_team_profile_photo')['url']; ?>" alt="<?php the_title(); ?> Profile Photo" /></div>
+
+				<?php endif; ?>
+
+				<div class="clearfix"></div>
+			</section><!-- .team-details -->
 
 			<?php include get_template_directory() . '/_/inc/modules/social-slider.php'; ?>
 

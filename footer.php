31075de801bac58423acc4600e5d13dd3fd402ae
BrainBrian <brian@brainbrian.com>
2015-04-13 14:17:58 -0700

    Update site to use Google Tag Manager
    - remove google analytics code, now applied via tag manager
    - remove AdRoll, integrating Criteo

--- footer.php
+++ footer.php
@@ -156,7 +156,7 @@
 
 	<!-- JavaScript includes -->
 <?php include '_/inc/footer-includes.php' ?>
-	
+
 	<!-- Init the main JS -->
 	<script type="text/javascript">
 		$(document).ready(function(){
@@ -190,34 +190,6 @@
 			p.src = '//assets.pinterest.com/js/pinit.js';
 			f.parentNode.insertBefore(p, f);
 		}(document));
-	</script>
-	<!-- AdRoll -->
-	<script type="text/javascript">
-		adroll_adv_id = "OPXHQUBUYRGNZHAWFBAHVE";
-		adroll_pix_id = "YFARZDWU5BEYPLORNYCKQI";
-		(function () {
-			var oldonload = window.onload;
-			window.onload = function(){
-				__adroll_loaded=true;
-				var scr = document.createElement("script");
-				var host = (("https:" == document.location.protocol) ? "https://s.adroll.com" : "http://a.adroll.com");
-				scr.setAttribute('async', 'true');
-				scr.type = "text/javascript";
-				scr.src = host + "/j/roundtrip.js";
-				((document.getElementsByTagName('head') || [null])[0] ||
-				document.getElementsByTagName('script')[0].parentNode).appendChild(scr);
-				if(oldonload){oldonload();}
-			};
-		}());
-	</script>
-	<!-- Google Analytics -->
-	<script type="text/javascript">
-		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-		ga('create', 'UA-10240628-1', 'auto');
-		ga('send', 'pageview');
 	</script>
 </body>
 </html>
--- header.php
+++ header.php
@@ -144,6 +144,15 @@ GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG         GGGGGG
 	?>
 </head>
 <body <?php body_class($bodyClass); ?>>
+	<!-- Tag Manager -->
+	<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-T8D6V8"
+	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-T8D6V8');</script>
+	<!-- End Tag Manager -->
 	<div id="page">
 		<header id="masthead" class="site-header" role="banner">
 			<div class="header-wrapper" itemscope itemtype="http://schema.org/Organization">
